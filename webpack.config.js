037de7c48d38a6768617a363a9f078dc899d85bb
Oleg Yunin <oayunin@gmail.com>
2021-10-17 21:55:32 +0300

    -

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.tsx?$/,
-    //             use: 'ts-loader',
-    //             exclude: /node_modules/,
-    //         },
-    //     ],
-    // },
-    // resolve: {
-    //     extensions: ['.tsx', '.ts', '.js'],
-    // },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
